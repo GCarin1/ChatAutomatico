164e966ad872e5e2b9561b99c01c8dfb1d306a49
Guilherme Carini <carinigcontact@gmail.com>
2021-04-22 21:19:38 -0300

    inserindo services e arrumando controller

--- src/controller/SettingsController.ts
+++ src/controller/SettingsController.ts
@@ -1,20 +1,21 @@
 import {Request,Response} from 'express'
-import {getCustomRepository} from 'typeorm';
-import {SettingsRepository} from "../repositories/SettingsRepository"
+import { SettingServices } from '../services/SettingServices';
 
 class SettingsController{
     async create(request:Request,response:Response){
         const {chat,username}= request.body;
-    const settingsRepository = getCustomRepository(SettingsRepository);
+   
+        const settingServices= new SettingServices();
+     try{
+        const settings=await settingServices.create({chat,username})
+        return response.json(settings);
+     }catch(err){
+         return response.status(400).json({
+             message: err.message,
+         });
+     }
 
-    const settings = settingsRepository.create({
-        chat,
-        username,
-    });
-    await settingsRepository.save(settings);
-    return response.json(settings);
-}
-
-    }
+     }
+ }
 
 export{SettingsController}
