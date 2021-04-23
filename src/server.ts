1d5a762d697c6ff0d9314c0d82e3a97976a2e0b6
Guilherme Carini <carinigcontact@gmail.com>
2021-04-23 11:54:16 -0300

    iniciando socket e adicionando frontend

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,30 @@
-import express from "express";
+import express, { request, response } from "express";
+import{createServer}from 'http';
+import{Server,Socket}from 'socket.io';
 import "./database";
 import {routes} from "./routes"
+import path from 'path';
+import { View } from "typeorm/schema-builder/view/View";
+import { Repository } from "typeorm";
+
 const app = express();
+
+app.use(express.static(path.join(__dirname,"..","public")));
+app.set("views",path.join(__dirname,"..","public"));
+app.engine("html",require("ejs").renderFile);
+app.set("view engine","html");
+
+
+app. get ("/pages/client",(request,response)=>{
+    return response.render("html/client.html")
+});
+const http = createServer(app);
+const io = new Server(http);
+
+io.on("connection",(socket:Socket)=>{
+    console.log("Se conectou",socket.id);
+});
 
 app.use(express.json());
 app.use(routes);
-app.listen(3333,()=>console.log("server run"));
+http.listen(3333,()=>console.log("server run"));
